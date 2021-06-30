8d9f5af78397bf82f2a9a39bbc55b0cdc29ffcb9
Anshil <anshilp@karunya.edu.in>
2021-07-01 00:53:47 +0530

    Binded the app with the server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
-const server = http.createServer();
+const app = require('./app')
+const server = http.createServer(app);
 
 server.listen(port);
