7d2d7f5c30f1957d2330af83e05d3ac38192dc2d
Anshil <anshilp@karunya.edu.in>
2021-07-01 16:05:28 +0530

    Added the View route

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const viewRoutes = require('./api/routes/view');
 
-app.use((req,res,next)=>{
-res.status(200).json({
-    message : "test server"
+
+
+// main routes
+app.use('/users/view', viewRoutes);
+
+
+
+
+//error handlers
+app.use((req,res,next) =>{
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error); // forwards the error request
 });
+
+
+app.use((error,req,res,next) =>{
+    res.status(error.status || 500 );
+    res.json({
+        error:{
+            message : error.message
+        }
+    });
 });
 
 module.exports = app;
