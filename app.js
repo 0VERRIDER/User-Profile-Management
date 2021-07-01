bd67df940e2508d53e4bba5555a9339c518d1e26
Anshil <anshilp@karunya.edu.in>
2021-07-01 16:20:38 +0530

    Fixed CORS issues

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const viewRoutes = require('./api/routes/view');
 //body parser
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-
+//cors fix
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin',"*");//allowed access to all client
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+});
 // main routes
 app.use('/users/view', viewRoutes);
 
