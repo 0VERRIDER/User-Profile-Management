c16a47a6188b2cc62c3354c6d0d3ea23a6d10e2a
0VERRIDER <anshilp@karunya.edu.in>
2021-07-05 16:46:51 +0530

    Implemented : account creation api

--- api/routes/create.js
+++ api/routes/create.js
@@ -10,14 +10,14 @@ router.get('/',(req,res,next) => {
         role : req.body.role
     }
     res.status(200).json({
-        user_id : "Hi ",
-        content : user
+        message : "Hi there, its a POST API",
     });
 });
 
 //POSt request handler for the create method
 
 router.post('/',(req,res,next) => {
+    try{
     const user = new Users({
         _id: mongoose.Types.ObjectId(),
         first_name: req.body.first_name,
@@ -31,15 +31,32 @@ router.post('/',(req,res,next) => {
         updated_time:  Date.now()
     });
     user.save().then(result => {
+       
         res.status(201).json({
             message : "Thank you "+user.first_name+". Account registred successfully.",
             result: result
         });
     }).catch( err =>{
+        if(err.keyPattern['email']>0){
         res.status(500).json({
-            error: err
+            error: "User already exist with the mail id ' "+user.email+ "'"
         });
+    }
+        else{
+            res.status(500).json({
+                error: err
+            });
+        }
+    
     });
+   
+}
+catch(err){
+    res.status(500).json({
+        error: err
+    });
+}
+   
 
     
 });
