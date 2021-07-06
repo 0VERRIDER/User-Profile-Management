de7d6b08287cfc51d3c41c3cf3c2da2076685666
0VERRIDER <anshilp@karunya.edu.in>
2021-07-07 03:52:21 +0530

    Implemented : Update user api

---
+++ api/routes/update.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router   = express.Router();
+const Users = require('./models/users');
+
+router.post('/',(req,res,next) => {
+    let errorCode=404;
+
+    try{
+    const email = req.body.email;
+    const body = req.body;
+    if(body.hasOwnProperty("updated_time") || body.hasOwnProperty("created_time") || body.hasOwnProperty("id") ){
+        errorCode = 403;
+        throw "Invalid entries found!";
+    }
+    else if(!body.hasOwnProperty("email")){
+        errorCode = 404;
+        throw "User not found!";
+    }
+    body['updated_time'] = Date.now();
+
+    Users.updateOne({email:email},body).exec()
+    .then(data =>{
+        res.status(200).json({message : email+"'s account has been updated.",
+    body: body
+    });
+
+        
+    }).catch(err =>{
+        res.status(404).json({error: err});
+    });
+
+    }
+    catch(err){
+        res.status(errorCode).json({error: err});
+
+    }
+});
+
+
+module.exports = router;
