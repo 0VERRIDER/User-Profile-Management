2fcbc66c87ee835d37a1559db6b676eea4d0c3b1
0VERRIDER <anshilp@karunya.edu.in>
2021-07-11 23:27:35 +0530

    Changed : Auth method

--- api/routes/auth/auth.js
+++ api/routes/auth/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 module.exports = (req,res,next)=>{
     try{    
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.cookies.token;
         const decoded = jwt.verify(token,"secret",null);
         req.userData = decoded;
         next();
